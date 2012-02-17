e83f0f7a4b50561dbdf25bfae16a02d094f75ca3
Jean-Michel Garnier <jean-michel@21croissants.com>
2012-02-17 10:50:18 +0100

    TODO notes about suggested Refactoring. Let's TALK before you do anything in case my notes are not clear enough ;)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,11 +29,14 @@ module ApplicationHelper
 
   # Which role are you playing ?
   def is_admin
-    current_user.role == 'admin' ? true : false
+    current_user.role == 'admin' ? true : false  # TODO You can probably safely remove the ? true : false
   end
 
-  # Can we show forms ?
+  # Can we show forms ?  TODO Why the method name has a different name? Please rename or remove the comment
   def can_edit_member
+    # TODO Refactor => this is a crude copy paste from MembersController#show , please DRY
+    # TODO Refactor => If you were unit testing your helpers, you would extract curent_user to a metthod param
+    # TODO Refactor => Keep Your Views STUPID! Extract this logic to the User model
     (current_user.role == 'admin' || current_user.member.id == @member.id)? true : false
   end
 
