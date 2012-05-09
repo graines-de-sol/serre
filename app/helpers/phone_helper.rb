5d78ed1ad3cd7a3d0e02568873fb8441e2910f0f
Etienne Depaulis <etienne.depaulis@gmail.com>
2012-05-09 21:03:03 +0200

    Phone Number Spec

---
+++ app/helpers/phone_helper.rb
@@ -0,0 +1,20 @@
+module PhoneHelper
+  
+  # Dummy phone numbers formating
+  def phone_number(number)
+    regex = /^(\+\d{2}\s?\d{1}\s?|\d{2})\s?(\d{2})\s?(\d{2})\s?(\d{2})\s?(\d{2})$/i
+    if m = number.match(regex)     
+#      raise m.inspect
+      regex = /^(\+\d{2})(\d){1}$/i
+      if mm = m[1].strip.match(regex)     
+        return "#{mm[1]} #{mm[2]} #{m[2]} #{m[3]} #{m[4]} #{m[5]}"
+      else
+        return "#{m[1].strip} #{m[2]} #{m[3]} #{m[4]} #{m[5]}"
+      end
+
+      
+    end
+      
+    return number
+  end
+end
