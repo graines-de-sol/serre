dcda32c9790d864c403198e0a96a370806891abf
Damien Mathieu <42@dmathieu.com>
2012-05-09 22:33:29 +0200

    go away, you useless method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,16 +32,6 @@ module ApplicationHelper
     (current_user.role == 'admin' && !current_user.view_as_user) ? true : false
   end
 
-  # Generate I18n for JS
-  def translate_to_client(strings)
-    translations = "<script>"
-    translations << "if(!I18n){var I18n = {};};"
-    strings.each do |s|
-      translations << 'I18n["'+s+'"] = "'+t('warnings.'+s)+'";'
-    end
-    translations << "</script>"
-  end
-
   # Display socials networks links and icon if profile exists
   def show_social_profiles(networks)
     out = ''
