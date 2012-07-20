6df1c3de0f01f4cdd233e59de9ce51e6ef0f3f47
guillaume <gbarillot@gmail.com>
2012-07-20 09:35:48 +0200

    fix deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ namespace :deploy do
 
   task :symlink_shared do
     run "ln -s #{shared_path}/config/database.yml #{release_path}/config/"
-    run "ln -s #{shared_path}/medias/ #{release_path}/medias"
+    run "rm -rf #{release_path}/medias && ln -s #{shared_path}/medias #{release_path}/medias"
     run "rm -rf #{release_path}/public && ln -s #{shared_path}/public #{release_path}/public"
   end
 
