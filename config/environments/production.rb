d793a0128f241c5b7a70aba4a0d2c95b463c8fbf
guillaume <gbarillot@gmail.com>
2012-07-23 21:59:51 +0200

    fix f*** bundler issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Refuge::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #false
 
   # Generate digests for assets URLs
   config.assets.digest = true
