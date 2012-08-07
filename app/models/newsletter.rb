0832e3c7c80aa1185624acf71ab37490028de963
guillaume <gbarillot@gmail.com>
2012-08-07 14:52:53 +0200

    fiw newsletter bug

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -3,10 +3,10 @@ class Newsletter < ActiveRecord::Base
   # Fetch all published news since last newsletter sending
   def self.latest_news
     last_sent = self.where(['sent_on IS NOT NULL']).last
+    last_sent ? start_date = last_sent.sent_on : start_date = '2010-01-01 00:00:00'.to_date
 
-    if last_sent
-      Post.where(['created_at > ? AND published = ?', last_sent.sent_on, true]).order('created_at DESC').all
-    end
+    Post.where(['created_at > ? AND published = ?', start_date, true]).order('created_at DESC').all
+
   end
 
 end
