57145d73c030e8850e40fd143a818aa0a775c198
guillaume <gbarillot@gmail.com>
2012-08-30 17:57:50 +0200

    fix order in blog categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,14 +5,14 @@ class Post < ActiveRecord::Base
 
   scope :last_posts, self.where(['published_at <= ?', Time.now]).order('published_at DESC').limit(5)
   scope :by_category, lambda{ |category_id|
-    self.where(['blog_category_id = ? AND published_at < ?', category_id, Time.now])
+    self.where(['blog_category_id = ? AND published_at <= ?', category_id, Time.now]).order('published_at DESC')
   }
   scope :by_month, lambda{ |year, month|
-    self.where(["YEAR(created_at) = ? AND MONTH(created_at) = ? AND published_at < ?", year, month, Time.now])
+    self.where(["YEAR(created_at) = ? AND MONTH(created_at) = ? AND published_at <= ?", year, month, Time.now]).order('published_at DESC')
   }
 
   def self.archives_by_month
-    self.where(['published_at < ?', Time.now]).order('published_at DESC').group_by{|p| p.published_at.beginning_of_month}
+    self.where(['published_at <= ?', Time.now]).order('published_at DESC').group_by{|p| p.published_at.beginning_of_month}
   end
 
 
