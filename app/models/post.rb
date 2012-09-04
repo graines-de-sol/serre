3a91acdffe2559f4cbb4b685f0e3dacd3f92c9f7
guillaume <gbarillot@gmail.com>
2012-09-05 00:16:10 +0200

    conf not loading in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   belongs_to :blog_category
 
-  scope :last_posts, self.where(['published_at <= ?', Time.now]).order('published_at DESC').limit($conf.max_post_on_index)
+  scope :last_posts, self.where(['published_at <= ?', Time.now]).order('published_at DESC').limit(10)
   scope :by_category, lambda{ |category_id|
     self.where(['blog_category_id = ? AND published_at <= ?', category_id, Time.now]).order('published_at DESC')
   }
