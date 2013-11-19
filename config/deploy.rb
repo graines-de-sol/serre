7f180a83d605c1db70bb2d67306ba6334c161f16
guillaume barillot <gbarillot@gmail.com>
2013-11-19 23:00:56 +0100

    add capistrano gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :scm, 'git'
 set :default_stage, 'production'
 set :branch, fetch(:branch, "master")
 set :normalize_asset_timestamps, false
+set :rvm_ruby_string, '1.9.3-p194'
 
 role :web, "ns382592.ovh.net"
 role :app, "ns382592.ovh.net"
